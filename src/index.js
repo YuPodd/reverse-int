da429d4fd2afb74560b52578ecb9bb4f377f0beb
Yuliya <yulia.podd@gmail.com>
2020-10-25 11:28:44 +0200

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString().split('').reverse().join('');
+   if (str.charAt(str.length -1) == '-'){
+    str = str.slice(0, -1);
+   }
+   return Number(str);
 }
